0979d2c448c6d473042ca2513e7fcf38d96c0118
hajungIm <ihj_ewha@ewhain.net>
2024-01-23 17:01:59 +0900

    feat: 포스트 수정 Service 구현

--- src/main/java/springstudy/sns/service/PostService.java
+++ src/main/java/springstudy/sns/service/PostService.java
@@ -26,4 +26,20 @@ public class PostService {
                 () -> new SnsApplicationException(ErrorCode.USER_NOT_FOUND, String.format("%s not founded", userName)));
         postEntityRepository.save(PostEntity.of(title, body, userEntity));
     }
+
+    @Transactional
+    public Post modify(String title, String body, String userName, Integer postId) {
+        UserEntity userEntity = userEntityRepository.findByUserName(userName).orElseThrow(
+                () -> new SnsApplicationException(ErrorCode.USER_NOT_FOUND, String.format("%s not founded", userName)));
+        // post exist
+        PostEntity postEntity = postEntityRepository.findById(postId).orElseThrow(() ->
+                new SnsApplicationException(ErrorCode.POST_NOT_FOUND, String.format("%s not founded", postId)));
+
+        // post permission
+        if (postEntity.getUser() != userEntity) {
+            throw new SnsApplicationException(ErrorCode.INVALID_PERMISSION, String.format("%s has no permission with %s", userName, postId));
+        }
+
+        return postEntity.updatePost(title, body);
+    }
 }
