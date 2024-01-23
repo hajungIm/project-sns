adb940cada9a77f0c4110e310884ddceb537e2be
hajungIm <ihj_ewha@ewhain.net>
2024-01-23 16:24:34 +0900

    feat: Security 예외 핸들링 추가

---
+++ src/main/java/springstudy/sns/exception/CustomAuthenticationEntryPoint.java
@@ -0,0 +1,18 @@
+package springstudy.sns.exception;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import springstudy.sns.controller.response.Response;
+
+public class CustomAuthenticationEntryPoint implements AuthenticationEntryPoint {
+    @Override
+    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
+        response.setContentType("application/json");
+        response.setStatus(ErrorCode.INVALID_TOKEN.getStatus().value());
+        response.getWriter().write(Response.error(ErrorCode.INVALID_TOKEN.name()).toStream());
+    }
+}
