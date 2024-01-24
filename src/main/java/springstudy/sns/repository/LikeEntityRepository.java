98a5303cc4ce162fadd1e114de676183e4505598
hajungIm <ihj_ewha@ewhain.net>
2024-01-24 16:26:21 +0900

    feat: LikeEntity 생성
    - 다른 엔티티 @NoArgsConstructor 접근 설정 추가

---
+++ src/main/java/springstudy/sns/repository/LikeEntityRepository.java
@@ -0,0 +1,18 @@
+package springstudy.sns.repository;
+
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+import springstudy.sns.model.entity.LikeEntity;
+import springstudy.sns.model.entity.PostEntity;
+import springstudy.sns.model.entity.UserEntity;
+
+@Repository
+public interface LikeEntityRepository extends JpaRepository<LikeEntity, Integer> {
+    Optional<LikeEntity> findByUserAndPost(UserEntity user, PostEntity post);
+
+    @Query(value = "SELECT COUNT(*) FROM LikeEntity entity WHERE entity.post =:post")
+    Integer countByPost(@Param("post") PostEntity post);
+}
