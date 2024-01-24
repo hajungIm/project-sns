376ce66ed442c3bbc082876de9a6c3d73c87a098
hajungIm <ihj_ewha@ewhain.net>
2024-01-24 16:27:32 +0900

    feat: 좋아요 누르기 기능

--- src/main/java/springstudy/sns/service/PostService.java
+++ src/main/java/springstudy/sns/service/PostService.java
@@ -8,8 +8,10 @@ import org.springframework.transaction.annotation.Transactional;
 import springstudy.sns.exception.ErrorCode;
 import springstudy.sns.exception.SnsApplicationException;
 import springstudy.sns.model.Post;
+import springstudy.sns.model.entity.LikeEntity;
 import springstudy.sns.model.entity.PostEntity;
 import springstudy.sns.model.entity.UserEntity;
+import springstudy.sns.repository.LikeEntityRepository;
 import springstudy.sns.repository.PostEntityRepository;
 import springstudy.sns.repository.UserEntityRepository;
 
@@ -19,6 +21,7 @@ public class PostService {
 
     private final PostEntityRepository postEntityRepository;
     private final UserEntityRepository userEntityRepository;
+    private final LikeEntityRepository likeEntityRepository;
 
     @Transactional
     public void create(String title, String body, String userName) {
@@ -67,5 +70,20 @@ public class PostService {
                 new SnsApplicationException(ErrorCode.USER_NOT_FOUND, String.format("%s not founded", userName)));
 
         return postEntityRepository.findAllByUser(userEntity, pageable).map(Post::fromEntity);
+    }
+
+    @Transactional
+    public void like(Integer postId, String userName) {
+        UserEntity userEntity = userEntityRepository.findByUserName(userName).orElseThrow(() ->
+                new SnsApplicationException(ErrorCode.USER_NOT_FOUND, String.format("%s not founded", userName)));
+        PostEntity postEntity = postEntityRepository.findById(postId).orElseThrow(() ->
+                new SnsApplicationException(ErrorCode.POST_NOT_FOUND, String.format("%s not founded", postId)));
+        // check liked -> throw
+        likeEntityRepository.findByUserAndPost(userEntity, postEntity).ifPresent(it -> {
+            throw new SnsApplicationException(ErrorCode.ALREADY_LIKED, String.format("userName %s already like post %d", userName, postId));
+        });
+
+        // like save
+        likeEntityRepository.save(LikeEntity.of(userEntity, postEntity));
     }
 }
